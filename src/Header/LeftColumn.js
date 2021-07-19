6ef863d3e5fd9249fbcdf54aa6906c91a11170e0
Mukesh Kumar <77101515+Programmer-Mukesh@users.noreply.github.com>
2021-07-19 14:37:35 +0530

    Update LeftColumn.js

--- src/Header/LeftColumn.js
+++ src/Header/LeftColumn.js
@@ -1,23 +1,15 @@
 import React from "react";
+import { LeftSideText } from "./constant";
 
-const LeftColumn = () => {
-  const P1 = [
-    "Enroll",
-    "The right care could",
-    "be right around the",
-    "corner!",
-  ];
-
-  return (
-    <div className="leftText">
-      <p className="boldPara">{P1[0]}</p>
-      <span>{P1[1]}</span>
-      <br />
-      <span>{P1[2]}</span>
-      <br />
-      <span>{P1[3]}</span>
-    </div>
-  );
-};
+const LeftColumn = () => (
+  <div className="leftText">
+    <p className="boldPara">{LeftSideText[0]}</p>
+    <span>{LeftSideText[1]}</span>
+    <br />
+    <span>{LeftSideText[2]}</span>
+    <br />
+    <span>{LeftSideText[3]}</span>
+  </div>
+);
 
 export default LeftColumn;
