72442dec88232c9dee626ef6258e42704780f64c
Mukesh Kumar <77101515+Programmer-Mukesh@users.noreply.github.com>
2021-07-19 22:57:55 +0530

    Update NewUser.js

--- src/Header/NewUser.js
+++ src/Header/NewUser.js
@@ -1,32 +1,26 @@
 import React from "react";
 import InputComponent from "./Input";
 import Select from "./Select";
+import { insideText, TextP, valueDob, assignClass } from "./constant";
 
-const NewUser = () => {
-  const insideText = ["Email () ", "Password"];
-  const TextP = "Complete these fields to achieve your benefits.";
-  const assignClass = "items";
-  const valueDob = ["Birth Month", "Day", "Year"];
-
-  return (
-    <div>
-      <InputComponent
-        addClass={assignClass}
-        type="text"
-        placeholder={insideText}
-      />
-      <div className="toggle">
-        <p className="formContent">{TextP}</p>
-        <input className="items" type="text" placeholder="Employee Id " />
-        <div>
-          <select className="items" name="Role">
-            <option value="Role">Role</option>
-          </select>
-        </div>
+const NewUser = () => (
+  <div>
+    <InputComponent
+      addClass={assignClass}
+      type="text"
+      placeholder={insideText}
+    />
+    <div className="toggle">
+      <p className="formContent">{TextP}</p>
+      <input className="items" type="text" placeholder="Employee Id " />
+      <div>
+        <select className="items" name="Role">
+          <option value="Role">Role</option>
+        </select>
       </div>
-      <Select values={valueDob} />
     </div>
-  );
-};
+    <Select values={valueDob} />
+  </div>
+);
 
 export default NewUser;
