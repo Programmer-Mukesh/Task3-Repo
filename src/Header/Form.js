6d91750c889ff3135c7b0ab4189cc42cccc83b15
Mukesh Kumar <77101515+Programmer-Mukesh@users.noreply.github.com>
2021-07-19 16:45:08 +0530

    Update Form.js

--- src/Header/Form.js
+++ src/Header/Form.js
@@ -2,51 +2,31 @@ import React, { useState } from "react";
 import ExistingUser from "./ExistingUser";
 import FormDesc from "./FormDesc";
 import NewUser from "./NewUser";
+import { FormText } from "./constant";
 
 const Form = () => {
-  const [showExistingUser, setshowExistingUser] = useState(false);
-  const [showNewUser, setshowNewUser] = useState(true);
+  const [IsLoggedin, setIsLoggedin] = useState(true);
 
   const onClick1 = () => {
-    if (setshowExistingUser(false)) {
-      setshowNewUser(false);
-      setshowExistingUser(true);
-    } else {
-      setshowNewUser(true);
-      setshowExistingUser(false);
-    }
+    setIsLoggedin(false);
   };
 
   const onClick2 = () => {
-    if (setshowNewUser(false)) {
-      setshowNewUser(true);
-      setshowExistingUser(false);
-    } else {
-      setshowNewUser(false);
-      setshowExistingUser(true);
-    }
+    setIsLoggedin(true);
   };
-
-  const Text = [
-    "Are you new to Care.com?",
-    "Yes",
-    "No",
-    "Enter your existing Care.com login information.",
-  ];
 
   return (
     <div className="formTxt">
-      <p>{Text[0]}</p>
+      <p>{FormText[0]}</p>
       <button className="formButton yes" onClick={onClick1}>
-        {Text[1]}
+        {FormText[1]}
       </button>
       <button className="formButton no" onClick={onClick2}>
-        {Text[2]}
+        {FormText[2]}
       </button>
-      <p>{Text[3]}</p>
+      <p>{FormText[3]}</p>
       <form className="form" action="">
-        {showNewUser ? <ExistingUser /> : null}
-        {showExistingUser ? <NewUser /> : null}
+        {IsLoggedin ? <NewUser /> : <ExistingUser />}
         <FormDesc />
       </form>
     </div>
