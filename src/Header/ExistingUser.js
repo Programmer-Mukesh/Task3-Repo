8d99d94428b66a93fd125477aecf93ccdbf3053b
Mukesh Kumar <77101515+Programmer-Mukesh@users.noreply.github.com>
2021-07-19 22:52:06 +0530

    Update ExistingUser.js

--- src/Header/ExistingUser.js
+++ src/Header/ExistingUser.js
@@ -1,40 +1,39 @@
 import React from "react";
 import InputComponent from "./Input";
 import Select from "./Select";
+import {
+  insideText1,
+  insideText2,
+  assignClass1,
+  assignClass2,
+  insideText,
+  valueDob,
+} from "./constant";
 
-const ExistingUser = () => {
-  const insideText1 = ["Employee ID ", "First Name", "Last Name"];
-  const insideText2 = ["Address ", "Zip"];
-  const assignClass1 = "items hiddenForm";
-  const assignClass2 = "items";
-  const insideText = ["Email () ", "Password"];
-  const valueDob = ["Birth Month", "Day", "Year"];
-
-  return (
-    <div className="hidden hiddenForm">
-      <InputComponent
-        addClass={assignClass1}
-        type="text"
-        placeholder={insideText1}
-      />
-      <div>
-        <select className="items hiddenForm" name="Role">
-          <option value="Role">Role</option>
-        </select>
-      </div>
-      <InputComponent
-        addClass={assignClass2}
-        type="text"
-        placeholder={insideText2}
-      />
-      <InputComponent
-        addClass={assignClass2}
-        type="text"
-        placeholder={insideText}
-      />
-      <Select values={valueDob} />
+const ExistingUser = () => (
+  <div className="hidden hiddenForm">
+    <InputComponent
+      addClass={assignClass1}
+      type="text"
+      placeholder={insideText1}
+    />
+    <div>
+      <select className="items hiddenForm" name="Role">
+        <option value="Role">Role</option>
+      </select>
     </div>
-  );
-};
+    <InputComponent
+      addClass={assignClass2}
+      type="text"
+      placeholder={insideText2}
+    />
+    <InputComponent
+      addClass={assignClass2}
+      type="text"
+      placeholder={insideText}
+    />
+    <Select values={valueDob} />
+  </div>
+);
 
 export default ExistingUser;
