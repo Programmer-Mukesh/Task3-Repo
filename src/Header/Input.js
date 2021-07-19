5cd266f831c3d0c4a1cf27ecb23a28452f718b1e
Mukesh Kumar <77101515+Programmer-Mukesh@users.noreply.github.com>
2021-07-19 14:23:24 +0530

    Update Input.js

--- src/Header/Input.js
+++ src/Header/Input.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Input = ({ classN, type, placeholder }) => (
+const InputComponent = ({ addClass, type, placeholder }) => (
   <div>
     {placeholder.map((x, index) => (
-      <input className={classN} type={type} placeholder={x} key={index} />
+      <input className={addClass} type={type} placeholder={x} key={index} />
     ))}
   </div>
 );
 
-export default Input;
+export default InputComponent;
