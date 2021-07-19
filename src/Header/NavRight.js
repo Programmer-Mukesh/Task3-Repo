9da752f591e49047f99cbb2a7d5a38a912761989
Mukesh Kumar <77101515+Programmer-Mukesh@users.noreply.github.com>
2021-07-19 22:56:45 +0530

    Update NavRight.js

--- src/Header/NavRight.js
+++ src/Header/NavRight.js
@@ -1,20 +1,18 @@
 import React from "react";
 import UsLogo from "./flag.png";
+import { P1 } from "./constant";
 
-const NavRight = () => {
-  const P1 = "Contact us | Change country";
-  return (
-    <div className=" rightNav">
-      <p>
-        {P1}
-        <span>
-          <img className="usLogo" src={UsLogo} alt="usFlag" />
-        </span>
-        <span className="rotateArrow"> › </span>
-      </p>
-      <button className="btnHead">Log in</button>
-    </div>
-  );
-};
+const NavRight = () => (
+  <div className=" rightNav">
+    <p>
+      {P1}
+      <span>
+        <img className="usLogo" src={UsLogo} alt="usFlag" />
+      </span>
+      <span className="rotateArrow"> › </span>
+    </p>
+    <button className="btnHead">Log in</button>
+  </div>
+);
 
 export default NavRight;
