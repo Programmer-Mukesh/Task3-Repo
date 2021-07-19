59961b46225ba398f554fd69cd87bc4f6527639d
Mukesh Kumar <77101515+Programmer-Mukesh@users.noreply.github.com>
2021-07-19 22:49:03 +0530

    Delete src/Footer directory

--- src/Footer/FooterConstant.js
+++
@@ -1,6 +0,0 @@
-export const P1 =
-  "Care.com is the world's largest online destination for care. We connect families with great caregivers and caring companies to help you be there for the ones you love .";
-export const P2 =
-  " Care.com does not employ , recommend or endorse any care provider or care seeker nor is it responsible for the conduct of any care provider or care seeker Care.com provides information and tools to help care seekers and care providers connect and make informed decisions . However , each individual is solely responsible for selecting an appropriate care provider or care seeker for themselves or their families and for complying with all applicable laws in connection with any employment relationship they establish . Care.com does not provide medical advice , diagnosis or treatment or engage in any conduct that requires a professional license .";
-export const P3 =
-  ' Care.com and " There for you are service marks or registered service marks of Care.com , Inc. © 2007-2015 Care.com , Inc. All rights reserved .';
